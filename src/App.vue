a9675d71fdf8dbe430cfe76bb88b3a20daf0657a
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-10-03 13:43:10 +0200

    chore: add some minimal css code

--- src/App.vue
+++ src/App.vue
@@ -11,48 +11,21 @@
     <router-view/>
     <footer>
       <p>
-        &copy; 2024 Token.io by Lucas
+        &copy; 2024 token.io
       </p>
-      <p>
-        End of year project for Holberton School's Fundamentals program.
-      </p>
+      <!-- <p>
+        End of year project for Holberton School's CS Fundamentals program.
+      </p> -->
     </footer>
 
   </div>
 </template>
 
-
-
-
 <script>
-
-// import WelcomePage from './components/WelcomePage.vue'
-
 export default {
   name: 'App',
-  // components: {
-  //   WelcomePage,
-  // }
 }
-
 </script>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 <style>
 #app {
@@ -62,5 +35,36 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+header {
+  background-color: #72a1cf;
+  color: white;
+  padding: 10px;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+}
+
+header nav {
+  margin-right: 20px;
+  padding: 10px;
+}
+
+footer {
+  background-color: #72a1cf;
+  color: white;
+  padding: 10px;
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+#app {
+  margin-top: 80px; /* Adjust this value if needed to avoid content being hidden behind the header */
 }
 </style>
