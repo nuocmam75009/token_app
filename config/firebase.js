c91b0b23886fc31c0fbe3ec56a9a2c1149fc38dd
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-10-08 15:54:12 +0200

    FIX

--- config/firebase.js
+++ config/firebase.js
@@ -1,20 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getfirestore } from "firebase/firestore";
+import FIREBASE_API_KEY, { FIREBASE_AUTH_DOMAIN }? FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID, FIREBASE_MEASUREMENT_ID from .env
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD6hgYrJu2ILq2u2N6dG0xP7oUBCRBRgNI",
-  authDomain: "token-io.firebaseapp.com",
+  apiKey: FIREBASE_API_KEY,
+  authDomain: FIREBASE_AUTH_DOMAIN,
   databaseURL: "https://token-io-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "token-io",
-  storageBucket: "token-io.appspot.com",
-  messagingSenderId: "88563639982",
-  appId: "1:88563639982:web:1da43ff887fb6f96de2f3f",
-  measurementId: "G-Z9GZFF4LW7"
+  projectId: FIREBASE_PROJECT_ID,
+  storageBucket: FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+  appId: FIREBASE_APP_ID,
+  measurementId: FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
