23bf9d041f8136edcd5d480dfa6c50c368486a7e
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-10-08 09:59:43 +0200

    added the contactus form/no styling.

--- src/components/ContactUsPage.vue
+++ src/components/ContactUsPage.vue
@@ -1,11 +1,51 @@
 <template>
     <h1>Contact us</h1>
-    </template>
+    <form accept-charset="UTF-8">
+        <div class="form-field">
+            <label for="name">Name:</label>
+            <input type="text" id="name" name="name" required>
+        </div>
+        <div class="form-field">
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+        </div>
+        <div class="form-field">
+            <label for="message">Message:</label>
+            <textarea id="message" name="message" required></textarea>
+        </div>
 
-    <script>
+        <button type="submit">Submit</button>
+    </form>
 
-    </script>
 
-    <style>
 
-    </style>
+
+
+
+</template>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script>
+
+</script>
+
+<style>
+
+</style>
