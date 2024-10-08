128ddb6e770f7a079a1faf5090126cc1f2077afa
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-10-08 15:55:33 +0200

    fixed the env vars

--- config/firebase.js
+++ config/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getfirestore } from "firebase/firestore";
-import FIREBASE_API_KEY, { FIREBASE_AUTH_DOMAIN }? FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID, FIREBASE_MEASUREMENT_ID from .env
+import { FIREBASE_API_KEY, FIREBASE_AUTH_DOMAIN, FIREBASE_PROJECT_ID, FIREBASE_STORAGE_BUCKET, FIREBASE_MESSAGING_SENDER_ID, FIREBASE_APP_ID, FIREBASE_MEASUREMENT_ID } from "@env";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
