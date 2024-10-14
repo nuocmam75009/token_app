3d72986cecb768457efcbdc2d01d6690d6a246d8
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-10-14 12:36:42 +0200

    Refactor storeconstants.js to add authentication and username state and mutations

--- src/store/storeconstants.js
+++ src/store/storeconstants.js
@@ -1,5 +1,5 @@
 /*
-    This file stores the getter and setter constants 
+    This file stores the getter and setter constants
 */
 
 
@@ -7,3 +7,22 @@ export const SET_AUTHENTICATION = '[mutations] set authentication';
 export const IS_USER_AUTHENTICATED = '[getters] is user authenticated';
 export const SET_USERNAME = '[mutations] set username';
 export const GET_USERNAME = '[getters] get username';
+
+const state = {
+    isAuthenticated: false,
+    username: '',
+}
+
+const mutations = {
+    [SET_AUTHENTICATION](state, value) {
+        state.isAuthenticated = value;
+    },
+    [SET_USERNAME](state, value) {
+        state.username = value;
+    },
+}
+
+export default {
+    state,
+    mutations,
+}
