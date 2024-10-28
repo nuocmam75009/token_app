a7ee71340b9c00cc80525f8dcd5b9b288869189a
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-10-28 08:49:14 +0100

    ident

--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -1,35 +1,33 @@
 // Welcome Page
 <template>
-        <v-container class="welcome">
-            <v-row>
+    <v-container class="welcome">
+        <v-row>
             <v-col>
                 <h1>Welcome to token.io</h1>
             </v-col>
-            </v-row>
-        </v-container>
-        <v-btn
+        </v-row>
+    </v-container>
+    <v-btn
         prepend-icon="mdi-account"
         to="/login">
         Login
-        </v-btn>
-        <v-btn
+    </v-btn>
+    <v-btn
         prepend-icon="mdi-account"
         to="/register">
         Register
-        </v-btn>
+    </v-btn>
 
-        <v-container>
-            <v-row>
-                <v-col cols="4" v-for="(v, index) in data" v-bind:key="index">
-                    <v-sheet class="text-center">
-                        {{ v }}
-                        Hello
-                    </v-sheet>
-                </v-col>
-            </v-row>
-        </v-container>
-
-
+    <v-container>
+        <v-row>
+            <v-col cols="4" v-for="(v, index) in data" v-bind:key="index">
+                <v-sheet class="text-center">
+                    {{ v }}
+                    Hello
+                </v-sheet>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script>
