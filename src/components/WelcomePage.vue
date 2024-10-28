e17831a437bda5ba3ddfc3b3b9d25416269de35f
nuocmam75009 <sayhi.kiseiju@gmail.com>
2024-10-28 08:44:43 +0100

    refactor the landingPage. Still needs some more styling to be done.

--- src/components/WelcomePage.vue
+++ src/components/WelcomePage.vue
@@ -1,13 +1,22 @@
 // Welcome Page
 <template>
-        <div class="welcome">
-            <h1>Welcome to token.io</h1>
-        </div>
-        <router-link to="/login">
-            <button class="start">
-            Start learning now!
-            </button>
-        </router-link>
+        <v-container class="welcome">
+            <v-row>
+            <v-col>
+                <h1>Welcome to token.io</h1>
+            </v-col>
+            </v-row>
+        </v-container>
+        <v-btn
+        prepend-icon="mdi-account"
+        to="/login">
+        Login
+        </v-btn>
+        <v-btn
+        prepend-icon="mdi-account"
+        to="/register">
+        Register
+        </v-btn>
 
         <v-container>
             <v-row>
